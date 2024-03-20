ced652bb43a8be3a94bdee06d77c4d97f5180cae
Maksim Litvinov <lmk478shade@gmail.com>
2024-03-20 23:27:07 +0300

    feat: solve getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -50,10 +50,17 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
-}
+function getFirstResolvedPromiseResult(promises) {
+  const promise = Promise.race(promises)
+    .then((value) => {
+      return Promise.resolve(value);
+    })
+    .catch(() => {
+      return Promise.reject();
+    });
 
+  return promise;
+}
 /**
  * Returns containing the value of the first promise of a resolved or a rejected.
  *
