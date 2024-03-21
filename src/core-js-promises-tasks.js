e6b82597e6d0bf32496235c41d9fd8aa2c2623dd
Maksim Litvinov <lmk478shade@gmail.com>
2024-03-22 00:59:36 +0300

    feat: solve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -151,8 +151,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((prev, promise) => {
+    return prev
+      .then((res) => {
+        return promise.then((val) => res + val);
+      })
+      .catch((err) => {
+        return err;
+      });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
